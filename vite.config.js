d2f3a200ef42825a62e955fa0cf2ef83a7e56419
Prince <pk20287022@gmail.com>
2024-09-14 22:07:49 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Achieve_Aura/",
+  // vite.config.js
+
+  base: "./", // Set this to match your deployment path
 });
